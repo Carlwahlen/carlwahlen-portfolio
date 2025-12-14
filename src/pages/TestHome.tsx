03d39bda4236a88d2c38497ce64dd462579a82d6
Carl Wahlen <carl@carlwahlen.com>
2025-12-14 21:00:27 +0100

    Position image with top edge at circle top and slightly right of center
    - Set object-position: calc(50% + 15px) 5%
    - 5% from top aligns image top with circle top edge
    - 15px right of center (55%) positions face slightly right
    - Ensures full face and hair fit in circle

--- src/pages/TestHome.tsx
+++ src/pages/TestHome.tsx
@@ -42,7 +42,7 @@ const TestHome: React.FC = () => {
                       alt="Carl Wahlen"
                       className="w-full h-full object-cover grayscale profile-image-responsive"
                       style={{ 
-                        objectPosition: '50% 35%',
+                        objectPosition: 'calc(50% + 15px) 5%',
                         objectFit: 'cover'
                       }}
                       loading="eager"
